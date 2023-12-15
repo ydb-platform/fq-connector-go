76cc2b6a5eb9f26e04b53a83ea802b407d68b2a7
Your Name <you@example.com>
2023-12-15 14:50:21 +0300

    fake change

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "connector",
 	Short: "Connector for external data sources",
-	// Run: func(cmd *cobra.Command, args []string) {},
+	//  Run: func(cmd *cobra.Command, args []string) {},
 }
 
 func init() {
