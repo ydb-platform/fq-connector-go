03a6dc75e55fff33d6e5bfe674f9294aaa984c00
Nikita Amelin <woodey@mr-nvme-testing-01.search.yandex.net>
2023-12-18 13:36:45 +0000

    YQ-2560: add ydb connection_manager files

---
+++ app/server/datasource/rdbms/ydb/schema_provider.go
@@ -0,0 +1,73 @@
+package ydb
+
+import (
+	"context"
+	"fmt"
+	"path"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table"
+	"github.com/ydb-platform/ydb-go-sdk/v3/table/options"
+	"github.com/ydb-platform/fq-connector-go/app/server/utils"
+	my_log "github.com/ydb-platform/fq-connector-go/library/go/core/log"
+	rdbms_utils "github.com/ydb-platform/fq-connector-go/app/server/datasource/rdbms/utils"
+	api_service_protos "github.com/ydb-platform/fq-connector-go/libgo/service/protos"
+)
+
+type schemaProvider struct {
+	typeMapper      utils.TypeMapper
+}
+var _ rdbms_utils.SchemaProvider = (*schemaProvider)(nil)
+
+func (f *schemaProvider) GetSchema(
+	ctx context.Context, 
+	logger my_log.Logger, 
+	conn rdbms_utils.Connection, 
+	request *api_service_protos.TDescribeTableRequest,
+) (*api_service_protos.TSchema, error) {
+	ydb_conn := conn.(*Connection)
+	db, err := ydb.Unwrap(ydb_conn.DB)
+	if err != nil {
+		return nil, fmt.Errorf("unwrap connection: %w", err)
+	}
+
+	desc := options.Description{}
+	prefix := path.Join(db.Name(), request.Table)
+	cl := db.Table()
+
+	err = cl.Do(
+		ctx,
+		func(ctx context.Context, s table.Session) error {
+			desc, err = s.DescribeTable(ctx, prefix)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("get table description: %w", err)
+	}
+
+	sb := rdbms_utils.NewSchemaBuilder(f.typeMapper, request.TypeMappingSettings)
+	for _, column := range desc.Columns {
+		if err := sb.AddColumn(column.Name, column.Type.String()); err != nil {
+			return nil, fmt.Errorf("add column to schema builder: %w", err)
+		}
+	}
+
+	schema, err := sb.Build(logger)
+	if err != nil {
+		return nil, fmt.Errorf("build schema: %w", err)
+	}
+
+	return schema, nil
+}
+
+func NewSchemaProvider(
+	typeMapper utils.TypeMapper,
+) rdbms_utils.SchemaProvider {
+	return &schemaProvider{
+		typeMapper: typeMapper,
+	}
+}
