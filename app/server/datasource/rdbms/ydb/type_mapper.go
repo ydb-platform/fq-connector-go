aea51ae556a1cbd9a01bff2ba962e7798f6d8f49
Amelin Nikita <woodey@ydb.tech>
2023-12-21 12:47:27 +0000

    YQ-2560: import change 1.0

--- app/server/datasource/rdbms/ydb/type_mapper.go
+++ app/server/datasource/rdbms/ydb/type_mapper.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/apache/arrow/go/v13/arrow/array"
 	"github.com/ydb-platform/fq-connector-go/app/server/utils"
-	api_service_protos "github.com/ydb-platform/fq-connector-go/libgo/service/protos"
+	api_service_protos "github.com/ydb-platform/fq-connector-go/api/service/protos"
 	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb"
 )
 
