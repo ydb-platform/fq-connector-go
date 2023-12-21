1503e30ad5a9a695a7e0562a03b3bd200ae52339
Amelin Nikita <woodey@ydb.tech>
2023-12-21 12:25:58 +0000

    YQ-2560: add constants

--- app/server/datasource/rdbms/ydb/type_mapper.go
+++ app/server/datasource/rdbms/ydb/type_mapper.go
@@ -17,6 +17,20 @@ type typeMapper struct {
 	isOptinal *regexp.Regexp
 }
 
+const BoolType = "Bool"
+const Int8Type = "Int8"
+const Uint8Type = "Uint8"
+const Int16Type = "Int16"
+const Uint16Type = "Uint16"
+const Int32Type = "Int32"
+const Uint32Type = "Uint32"
+const Int64Type = "Int64"
+const Uint64Type = "Uint64"
+const FloatType = "Float"
+const DoubleType = "Double"
+const StringType = "String"
+const Utf8Type = "Utf8"
+
 func (tm typeMapper) SQLTypeToYDBColumn(columnName, typeName string, rules *api_service_protos.TTypeMappingSettings) (*Ydb.Column, error) {
 	var (
 		ydbType *Ydb.Type
@@ -45,31 +59,31 @@ func makePrimitiveType(typeName string) (*Ydb.Type, error) {
 	// TODO: add all types support
 	// Reference table: https://ydb.yandex-team.ru/docs/yql/reference/types/
 	switch {
-	case typeName == "Bool":
+	case typeName == BoolType:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_BOOL}}, nil
-	case typeName == "Int8":
+	case typeName == Int8Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_INT8}}, nil
-	case typeName == "Uint8":
+	case typeName == Uint8Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_UINT8}}, nil
-	case typeName == "Int16":
+	case typeName == Int16Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_INT16}}, nil
-	case typeName == "Uint16":
+	case typeName == Uint16Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_UINT16}}, nil
-	case typeName == "Int32":
+	case typeName == Int32Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_INT32}}, nil
-	case typeName == "Uint32":
+	case typeName == Uint32Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_UINT32}}, nil
-	case typeName == "Int64":
+	case typeName == Int64Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_INT64}}, nil
-	case typeName == "Uint64":
+	case typeName == Uint64Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_UINT64}}, nil
-	case typeName == "Float":
+	case typeName == FloatType:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_FLOAT}}, nil
-	case typeName == "Double":
+	case typeName == DoubleType:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_DOUBLE}}, nil
-	case typeName == "String":
+	case typeName == StringType:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_STRING}}, nil
-	case typeName == "Utf8":
+	case typeName == Utf8Type:
 		return &Ydb.Type{Type: &Ydb.Type_TypeId{TypeId: Ydb.Type_UTF8}}, nil
 	default:
 		return nil, fmt.Errorf("convert type '%s': %w", typeName, utils.ErrDataTypeNotSupported)
@@ -152,29 +166,29 @@ func transformerFromSQLTypes(typeNames []string, ydbTypes []*Ydb.Type) (utils.Ro
 
 func makePrimitiveTransformers(typeName string) (any, func(acceptor any, builder array.Builder) error, error) {
 	switch typeName {
-	case "Bool":
+	case BoolType:
 		return new(bool), appendValueToArrowBuilder[bool, uint8, *array.Uint8Builder, utils.BoolConverter], nil
-	case "Int8":
+	case Int8Type:
 		return new(int8), appendValueToArrowBuilder[int8, int8, *array.Int8Builder, utils.Int8Converter], nil
-	case "Int16":
+	case Int16Type:
 		return new(int16), appendValueToArrowBuilder[int16, int16, *array.Int16Builder, utils.Int16Converter], nil
-	case "Int32":
+	case Int32Type:
 		return new(int32), appendValueToArrowBuilder[int32, int32, *array.Int32Builder, utils.Int32Converter], nil
-	case "Int64":
+	case Int64Type:
 		return new(int64), appendValueToArrowBuilder[int64, int64, *array.Int64Builder, utils.Int64Converter], nil
-	case "Uint8":
+	case Uint8Type:
 		return new(uint8), appendValueToArrowBuilder[uint8, uint8, *array.Uint8Builder, utils.Uint8Converter], nil
-	case "Uint16":
+	case Uint16Type:
 		return new(uint16), appendValueToArrowBuilder[uint16, uint16, *array.Uint16Builder, utils.Uint16Converter], nil
-	case "Uint32":
+	case Uint32Type:
 		return new(uint32), appendValueToArrowBuilder[uint32, uint32, *array.Uint32Builder, utils.Uint32Converter], nil
-	case "Uint64":
+	case Uint64Type:
 		return new(uint64), appendValueToArrowBuilder[uint64, uint64, *array.Uint64Builder, utils.Uint64Converter], nil
-	case "Float":
+	case FloatType:
 		return new(float32), appendValueToArrowBuilder[float32, float32, *array.Float32Builder, utils.Float32Converter], nil
-	case "Double":
+	case DoubleType:
 		return new(float64), appendValueToArrowBuilder[float64, float64, *array.Float64Builder, utils.Float64Converter], nil
-	case "Utf8":
+	case Utf8Type:
 		return new(string), appendValueToArrowBuilder[string, []byte, *array.BinaryBuilder, utils.StringToBytesConverter], nil
 	default:
 		return nil, nil, fmt.Errorf("unknown primitive type '%v'", typeName)
@@ -183,29 +197,29 @@ func makePrimitiveTransformers(typeName string) (any, func(acceptor any, builder
 
 func makeOptionalTransformers(typeName string) (any, func(acceptor any, builder array.Builder) error, error) {
 	switch typeName {
-	case "Bool":
+	case BoolType:
 		return new(*bool), appendValueToArrowBuilder[bool, uint8, *array.Uint8Builder, utils.BoolConverter], nil
-	case "Int8":
+	case Int8Type:
 		return new(*int8), appendValueToArrowBuilder[int8, int8, *array.Int8Builder, utils.Int8Converter], nil
-	case "Int16":
+	case Int16Type:
 		return new(*int16), appendValueToArrowBuilder[int16, int16, *array.Int16Builder, utils.Int16Converter], nil
-	case "Int32":
+	case Int32Type:
 		return new(*int32), appendValueToArrowBuilder[int32, int32, *array.Int32Builder, utils.Int32Converter], nil
-	case "Int64":
+	case Int64Type:
 		return new(*int64), appendValueToArrowBuilder[int64, int64, *array.Int64Builder, utils.Int64Converter], nil
-	case "Uint8":
+	case Uint8Type:
 		return new(*uint8), appendValueToArrowBuilder[uint8, uint8, *array.Uint8Builder, utils.Uint8Converter], nil
-	case "Uint16":
+	case Uint16Type:
 		return new(*uint16), appendValueToArrowBuilder[uint16, uint16, *array.Uint16Builder, utils.Uint16Converter], nil
-	case "Uint32":
+	case Uint32Type:
 		return new(*uint32), appendValueToArrowBuilder[uint32, uint32, *array.Uint32Builder, utils.Uint32Converter], nil
-	case "Uint64":
+	case Uint64Type:
 		return new(*uint64), appendValueToArrowBuilder[uint64, uint64, *array.Uint64Builder, utils.Uint64Converter], nil
-	case "Float":
+	case FloatType:
 		return new(*float32), appendValueToArrowBuilder[float32, float32, *array.Float32Builder, utils.Float32Converter], nil
-	case "Double":
+	case DoubleType:
 		return new(*float64), appendValueToArrowBuilder[float64, float64, *array.Float64Builder, utils.Float64Converter], nil
-	case "Utf8":
+	case Utf8Type:
 		return new(*string), appendValueToArrowBuilder[string, []byte, *array.BinaryBuilder, utils.StringToBytesConverter], nil
 	default:
 		return nil, nil, fmt.Errorf("unknown primitive type '%v'", typeName)
