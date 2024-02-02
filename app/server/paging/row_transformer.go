0249fc64e055e2f7e9b0254bd409738ddfa6c9fa
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-02-02 13:54:21 +0300

    Trivial pool with distinct task for every member of a row

--- app/server/paging/row_transformer.go
+++ app/server/paging/row_transformer.go
@@ -19,6 +19,11 @@ type RowTransformer[T Acceptor] interface {
 	GetAcceptors() []T
 }
 
+type transformationRequest struct {
+	ix      int
+	builder array.Builder
+}
+
 type RowTransformerDefault[T Acceptor] struct {
 	// The row data itself.
 	acceptors []T
@@ -27,6 +32,9 @@ type RowTransformerDefault[T Acceptor] struct {
 	// Sometimes row contains more data than necessary.
 	// This array specifies what particular row items to convert into the columnar format.
 	wantedColumnIDs []int
+
+	requestChan chan transformationRequest
+	errChan     chan error
 }
 
 func (rt *RowTransformerDefault[T]) AppendToArrowBuilders(builders []array.Builder) error {
@@ -39,13 +47,14 @@ func (rt *RowTransformerDefault[T]) AppendToArrowBuilders(builders []array.Build
 			}
 		}
 	} else {
-		for i, acceptor := range rt.acceptors {
-			if err := rt.appenders[i](acceptor, builders[i]); err != nil {
-				return fmt.Errorf(
-					"append acceptor %#v of %d column to arrow builder %#v: %w",
-					acceptor, i, builders[i], err)
-			}
-		}
+		// for i, acceptor := range rt.acceptors {
+		// 	if err := rt.appenders[i](acceptor, builders[i]); err != nil {
+		// 		return fmt.Errorf(
+		// 			"append acceptor %#v of %d column to arrow builder %#v: %w",
+		// 			acceptor, i, builders[i], err)
+		// 	}
+		// }
+		return rt.enqueue(builders)
 	}
 
 	return nil
@@ -55,14 +64,57 @@ func (rt *RowTransformerDefault[T]) SetAcceptors(acceptors []T) { rt.acceptors =
 
 func (rt *RowTransformerDefault[T]) GetAcceptors() []T { return rt.acceptors }
 
+func (rt *RowTransformerDefault[T]) enqueue(builders []array.Builder) error {
+	for i := range rt.acceptors {
+		req := transformationRequest{ix: i, builder: builders[i]}
+
+		rt.requestChan <- req
+	}
+
+	var nonEmptyErr error
+	for i := 0; i < len(rt.acceptors); i++ {
+		if err := <-rt.errChan; err != nil {
+			nonEmptyErr = err
+		}
+	}
+
+	return nonEmptyErr
+}
+
+func (rt *RowTransformerDefault[T]) worker() {
+	for {
+		req, ok := <-rt.requestChan
+		if !ok {
+			return
+		}
+
+		err := rt.appenders[req.ix](rt.acceptors[req.ix], req.builder)
+		if err != nil {
+			err = fmt.Errorf(
+				"append acceptor %#v of %d column to arrow builder %#v: %w",
+				rt.acceptors[req.ix], req.ix, req.builder, err)
+		}
+
+		rt.errChan <- err
+	}
+}
+
 func NewRowTransformer[T Acceptor](
 	acceptors []T,
 	appenders []func(acceptor T, builder array.Builder) error,
 	wantedColumnIDs []int,
 ) RowTransformer[T] {
-	return &RowTransformerDefault[T]{
+	rt := &RowTransformerDefault[T]{
 		acceptors:       acceptors,
 		appenders:       appenders,
 		wantedColumnIDs: wantedColumnIDs,
+		requestChan:     make(chan transformationRequest, 1024),
+		errChan:         make(chan error, 1024),
 	}
+
+	for i := 0; i < 2; i++ {
+		go rt.worker()
+	}
+
+	return rt
 }
