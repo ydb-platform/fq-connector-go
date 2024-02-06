3b510009df91b0b16be9888ee7e39f13d13f6b6a
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-02-06 18:17:50 +0300

    Overall benchmark plot

--- app/bench/analyze.py
+++ app/bench/analyze.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 import pandas as pd
 
@@ -41,9 +42,8 @@ class ConnectorBenchmarkReport:
     def make_dataframe(self) -> pd.DataFrame:
         series = [self._read_report(path) for path in self.result_dir.glob("*.json")]
         df = pd.DataFrame(series).sort_values("columns")
-        df["version"] = self.version
         df["datasource"] = self.datasource
-        df["client"] = self.client
+        df["client"] = f"{self.client}\n{self.version}"
         return df
 
 
@@ -103,7 +103,7 @@ def make_dataframe_from_native_benchmarks() -> pd.DataFrame:
         NativeBenchmarkReport(
             filepath=Path("/home/vitalyisaev/troubles/YQ-2837/ch_native.json"),
             datasource="CH",
-            client="clickhouse-client",
+            client="clickhouse\nclient",
             rows_in_table=59986052,
         ),
         NativeBenchmarkReport(
@@ -118,43 +118,98 @@ def make_dataframe_from_native_benchmarks() -> pd.DataFrame:
     return df
 
 
-def draw_subplot(
-    df_: pd.DataFrame, label: str, y_column: str, ax: matplotlib.figure.Figure
-) -> matplotlib.figure.Figure:
-    ax.set_ylabel(label)
-    ax.set_xlabel("Number of columns to SELECT")
+# def draw_subplot(
+#     df_: pd.DataFrame, label: str, y_column: str, ax: matplotlib.figure.Figure
+# ) -> matplotlib.figure.Figure:
+#     ax.set_ylabel(label)
+#     ax.set_xlabel("Number of columns to SELECT")
+#
+#     keys = {
+#         "baseline": "red",
+#         "optimized": "blue",
+#     }
+#
+#     for key, color in keys.items():
+#         df = df_.loc[df_["key"] == key]
+#         ax.plot(df["columns"], df[y_column], color=color, label=key)
+#
+#     return ax
+#
+#
+# def draw_plot(df: pd.DataFrame) -> pd.Series:
+#     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
+#     fig.subplots_adjust(bottom=0.25, wspace=0.5)
+#     draw_subplot(df, "Throughput, MB/sec", "bytes_internal_rate", axes[0])
+#     draw_subplot(df, "Throughput, rows/sec", "rows_rate", axes[1])
+#     ax = draw_subplot(df, "CPU Utilization, %", "cpu_utilization", axes[2])
+#
+#     handles, labels = ax.get_legend_handles_labels()
+#     fig.legend(handles, labels, loc="lower right")
+#     fig.suptitle("Reading TPC-H S-10 Lineitem from PostgreSQL", fontsize=14)
+#
+#     fig.savefig("report.png")
 
-    keys = {
-        "baseline": "red",
-        "optimized": "blue",
-    }
 
-    for key, color in keys.items():
-        df = df_.loc[df_["key"] == key]
-        ax.plot(df["columns"], df[y_column], color=color, label=key)
+# def draw_subplot(
+#     df: pd.DataFrame, title: str, ax: matplotlib.figure.Figure
+# ) -> matplotlib.figure.Figure:
 
-    return ax
+#     pass
 
 
-def draw_plot(df: pd.DataFrame) -> pd.Series:
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-    fig.subplots_adjust(bottom=0.25, wspace=0.5)
-    draw_subplot(df, "Throughput, MB/sec", "bytes_internal_rate", axes[0])
-    draw_subplot(df, "Throughput, rows/sec", "rows_rate", axes[1])
-    ax = draw_subplot(df, "CPU Utilization, %", "cpu_utilization", axes[2])
-
-    handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc="lower right")
-    fig.suptitle("Reading TPC-H S-10 Lineitem from PostgreSQL", fontsize=14)
-
-    fig.savefig("report.png")
+@ticker.FuncFormatter
+def million_formatter(x, pos):
+    return "%.1f M" % (x / 1e6)
 
 
-def main():
+def draw_overall_plot(src: pd.DataFrame):
+    # max table width
+    df = src[src["columns"] == src["columns"].max()]
+
+    fig, axes = plt.subplots(ncols=2, nrows=1, sharey=True)
+    fig.suptitle("Reading TPC-H S-10 Lineitem table", fontsize=14)
+
+    once = False
+
+    for i, datasource in enumerate(pd.unique(df["datasource"])):
+        datasource_df = df[df["datasource"] == datasource]
+        print(datasource_df)
+
+        colors = [
+            "blue" if "connector" in client else "red"
+            for client in datasource_df["client"]
+        ]
+
+        ax = axes[i]
+        ax.bar(
+            datasource_df["client"],
+            datasource_df["rows_rate"],
+            align="center",
+            color=colors,
+            width=0.3,
+        )
+
+        if not once:
+            ax.set_ylabel("Throughput, rows/sec")
+            ax.yaxis.set_major_formatter(million_formatter)
+            once = True
+
+        ax.set_title(datasource)
+
+    fig.savefig("overall_plot.png")
+
+
+def make_overall_dataframe() -> pd.DataFrame:
     df1 = make_dataframe_from_connector_benchmarks()
     df2 = make_dataframe_from_native_benchmarks()
     df = pd.concat([df1, df2])
+    return df
+
+
+def main():
+    df = make_overall_dataframe()
     print(df)
+    draw_overall_plot(df)
 
 
 if __name__ == "__main__":
