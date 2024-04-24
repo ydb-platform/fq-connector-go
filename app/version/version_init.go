eecafd44cfee4f2368eb38e4824e92bcfae509de
skywalker-jpg <sufiianovtim@ydb.tech>
2024-04-24 16:32:56 +0300

    [fix] build

---
+++ app/version/version_init.go
@@ -0,0 +1,17 @@
+
+package version
+
+func init() {
+	tag = "v0.2.20-14-g0ad442c"
+	author = "skywalker-jpg"
+	commitHash = "0ad442c594fad69618717ed73e85acd383b434b5"
+	branch = "YQ-2811"
+	commitDate = "2024-04-24 16:21:37"
+	commitMessage = "[fix] build"
+	username = "/var/folders/0b/5cjgwgj54lz9qc273kyk8rnm0000gn/T/go-build2443916394/b001/exe/version"
+	buildLocation = "/Users/sufiianovtim/fq-connector-go"
+	hostname = "sufiianovtim-o"
+	hostInfo = "Darwin"
+	pathToGo = "/usr/local/bin/go"
+	goVersion = "go version go1.22.2 darwin/amd64"
+}
