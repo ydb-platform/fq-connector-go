990e9f2020e26f63305e9782e323ee373747dde2
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2024-04-26 19:54:51 +0500

    chore: explicitly set schema to default value

--- app/server/datasource/rdbms/mysql/get_query_and_args.go
+++ app/server/datasource/rdbms/mysql/get_query_and_args.go
@@ -6,8 +6,9 @@ import (
 
 func GetQueryAndArgs(request *api_service_protos.TDescribeTableRequest) (string, []any) {
 	// opts := request.GetDataSourceInstance().GetMysqlOptions().GetSchema()
-	query := "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = ?"
-	args := []any{request.Table}
+	// TODO: pass table_schema using mysql options. For now fallback to db name, which is default
+	query := "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = ? AND table_schema = ?"
+	args := []any{request.Table, request.GetDataSourceInstance().Database}
 
 	return query, args
 }
