8e7cbe0fb721cf4ef28d97f94b973b3def11ec93
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2024-05-22 02:52:12 +0500

    chore: add missing comment in get_query_and_args

--- app/server/datasource/rdbms/mysql/get_query_and_args.go
+++ app/server/datasource/rdbms/mysql/get_query_and_args.go
@@ -7,14 +7,15 @@ import (
 func GetQueryAndArgs(request *api_service_protos.TDescribeTableRequest) (string, []any) {
 	// opts := request.GetDataSourceInstance().GetMysqlOptions().GetSchema()
 	// TODO: pass table_schema using mysql options. For now fallback to db name, which is default
-	// TODO:
+	// TODO: do not add 'unsigned' and 'nullable' modifiers to column type and use the driver-provided
+	// fields instead.
 	query := `SELECT column_name, CONCAT(
 		IF(COLUMN_TYPE LIKE '%unsigned', CONCAT(DATA_TYPE, ' ', 'unsigned'),
 		DATA_TYPE),
 		' ',
 		IF(IS_NULLABLE = 'YES', 'nullable', '')
-	) AS DATA_TYPE FROM information_schema.columns WHERE table_name = ? AND table_schema = ?
-`
+	) AS DATA_TYPE FROM information_schema.columns WHERE table_name = ? AND table_schema = ?`
+
 	args := []any{request.Table, request.GetDataSourceInstance().Database}
 
 	return query, args
