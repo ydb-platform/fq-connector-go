61ca92fb3e6f406ba186ccf5f03aac1f2d5ab045
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2024-05-23 06:03:26 +0500

    chore: add comment about mysql schemas and dbs

--- app/server/datasource/rdbms/mysql/get_query_and_args.go
+++ app/server/datasource/rdbms/mysql/get_query_and_args.go
@@ -6,6 +6,8 @@ import (
 
 func GetQueryAndArgs(request *api_service_protos.TDescribeTableRequest) (string, []any) {
 	// TODO: do not add 'unsigned' modifiers to column type and use the driver-provided fields instead.
+	// In MySQL schema and database are basically the same thing. So we can safely pass dbname as
+	// `schema_name` when quering `information_schema`.
 	query := `SELECT column_name, column_type FROM information_schema.columns
 		WHERE table_name = ? AND table_schema = ?`
 
