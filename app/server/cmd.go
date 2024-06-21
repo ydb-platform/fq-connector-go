c3938c3fbe602c68e77a5b2f5b03ace146e44ce2
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-06-21 17:57:13 +0300

    Flag uint32

--- app/server/cmd.go
+++ app/server/cmd.go
@@ -3,9 +3,12 @@ package server
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
+	app_config "github.com/ydb-platform/fq-connector-go/app/config"
 	"github.com/ydb-platform/fq-connector-go/app/server/config"
 	"github.com/ydb-platform/fq-connector-go/common"
 )
@@ -21,15 +24,39 @@ var Cmd = &cobra.Command{
 	},
 }
 
-const configFlag = "config"
+const (
+	configFlag    = "config"
+	connectorPort = "connector-port"
+	metricsPort   = "metrics-port"
+	pprofPort     = "pprof-port"
+)
 
 func init() {
-	Cmd.Flags().StringP(configFlag, "c", "", "path to server config file")
+	Cmd.Flags().StringP(configFlag, "c", "", "Path to server config file")
+	Cmd.Flags().Uint32(connectorPort, 2130, "Connector GRPC server port")
+	Cmd.Flags().Uint32(metricsPort, 8766, "Metrics HTTP server port")
+	Cmd.Flags().Uint32(pprofPort, 6060, "Go pprof HTTP server port")
 
 	if err := Cmd.MarkFlagRequired(configFlag); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func flagToUint32() {
+
+}
+
+func overrideConfigWithFlags(cfg *app_config.TServerConfig, flags *pflag.FlagSet) error {
+	flags.Visit(func(f *pflag.Flag) {
+		var err error
+		switch f.Name {
+		case connectorPort:
+			cfg.ConnectorServer.Endpoint.Port, err = strconv.Atoi(f.Value.String())
+		case metricsPort:
+		case pprofPort:
+		}
+	})
 }
 
 func runFromCLI(cmd *cobra.Command, _ []string) error {
