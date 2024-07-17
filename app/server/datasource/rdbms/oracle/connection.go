e1e808ff158bf3a65644cd7d0a2787c1a469d803
Grigorii Papashvili <papashviliga@ydb.tech>
2024-07-17 23:19:27 +0000

    fix type in comments

--- app/server/datasource/rdbms/oracle/connection.go
+++ app/server/datasource/rdbms/oracle/connection.go
@@ -29,8 +29,8 @@ func (c Connection) Query(ctx context.Context, query string, args ...any) (rdbms
 	for i := 0; i < len(args); i++ {
 		valueArgs[i].Value = args[i]
 		// TODO YQ-3455: research
-		// 	for some reason queyr works with all Ordinal = 0
-		// 	Golang docs states, what Ordinal is used, when Name is not set. Setted always. And starts with 1:
+		// 	for some reason query works with all Ordinal = 0
+		// 	Golang docs states, that Ordinal is used, when Name is not set. Setted always. And starts with 1:
 		// 		https://pkg.go.dev/database/sql/driver#NamedValue
 		valueArgs[i].Ordinal = i + 1
 	}
