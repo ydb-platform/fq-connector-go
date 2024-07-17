3513f1fd936f62f365c03530a56449b2977e2578
Grigorii Papashvili <papashviliga@ydb.tech>
2024-07-17 21:59:07 +0000

    removing comments from table_metadata_query.go

--- app/server/datasource/rdbms/oracle/table_metadata_query.go
+++ app/server/datasource/rdbms/oracle/table_metadata_query.go
@@ -5,9 +5,6 @@ import (
 )
 
 func TableMetadataQuery(request *api_service_protos.TDescribeTableRequest) (string, []any) {
-	// opts := request.GetDataSourceInstance().GetPgOptions().GetSchema()
-	// TODO: maybe uppercase table_name
-	// TODO: check synonyms tables and tables from other schemes
 	query := "SELECT column_name, data_type FROM user_tab_columns WHERE table_name = :1"
 	args := []any{request.Table} // , opts}
 
