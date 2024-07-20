986e442b360c7bbe3afb09192ec7c42ad14c96bb
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-07-20 19:16:51 +0300

    YDB: fix table path construction (#150)

--- app/server/datasource/rdbms/clickhouse/sql_formatter.go
+++ app/server/datasource/rdbms/clickhouse/sql_formatter.go
@@ -83,6 +83,10 @@ func (sqlFormatter) SanitiseIdentifier(ident string) string {
 	return sanitizedIdent
 }
 
+func (f sqlFormatter) FormatFrom(tableName string) string {
+	return f.SanitiseIdentifier(tableName)
+}
+
 func NewSQLFormatter() rdbms_utils.SQLFormatter {
 	return sqlFormatter{}
 }
