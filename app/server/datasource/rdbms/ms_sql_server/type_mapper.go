4e3507c5c53ff229c0304c488a9ca87b5e410c80
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-07 17:57:42 +0300

    Initialized MS SQL Docker container

--- app/server/datasource/rdbms/ms_sql_server/type_mapper.go
+++ app/server/datasource/rdbms/ms_sql_server/type_mapper.go
@@ -46,13 +46,13 @@ func (typeMapper) SQLTypeToYDBColumn(columnName, typeName string, rules *api_ser
 		// Float may store either 4 or 8 bytes
 		// https://learn.microsoft.com/ru-ru/sql/t-sql/data-types/float-and-real-transact-sql?view=sql-server-ver16#remarks
 		ydbType = common.MakePrimitiveType(Ydb.Type_DOUBLE)
-	case "binary", "varbinary":
+	case "binary", "varbinary", "image":
 		ydbType = common.MakePrimitiveType(Ydb.Type_STRING)
 	case "char", "varchar", "text":
 		ydbType = common.MakePrimitiveType(Ydb.Type_STRING)
 	case "nchar", "nvarchar", "ntext":
 		ydbType = common.MakePrimitiveType(Ydb.Type_UTF8)
-	case "date", "time", "smalldatetime", "datetime", "datetime2", "datetimeoffset":
+	case "date", "time", "smalldatetime", "datetime", "datetime2":
 		return nil, fmt.Errorf("convert type '%s': %w", typeName, common.ErrDataTypeNotSupported)
 	default:
 		return nil, fmt.Errorf("convert type '%s': %w", typeName, common.ErrDataTypeNotSupported)
@@ -98,7 +98,7 @@ func transformerFromSQLTypes(types []string, ydbTypes []*Ydb.Type, cc conversion
 		case "FLOAT":
 			acceptors = append(acceptors, new(*float64))
 			appenders = append(appenders, makeAppender[float64, float64, *array.Float64Builder](cc.Float64()))
-		case "BINARY", "VARBINARY":
+		case "BINARY", "VARBINARY", "IMAGE":
 			acceptors = append(acceptors, new(*[]byte))
 			appenders = append(appenders, func(acceptor any, builder array.Builder) error {
 				cast := acceptor.(**[]byte)
@@ -110,10 +110,13 @@ func transformerFromSQLTypes(types []string, ydbTypes []*Ydb.Type, cc conversion
 
 				return nil
 			})
-		case "CHAR", "VARCHAR", "NCHAR", "NVARCHAR", "TEXT":
+		case "CHAR", "VARCHAR", "TEXT":
+			acceptors = append(acceptors, new(*[]byte))
+			appenders = append(appenders, makeAppender[[]byte, []byte, *array.BinaryBuilder](cc.Bytes()))
+		case "NCHAR", "NVARCHAR", "NTEXT":
 			acceptors = append(acceptors, new(*string))
 			appenders = append(appenders, makeAppender[string, string, *array.StringBuilder](cc.String()))
-		case "date", "time", "smalldatetime", "datetime", "datetime2", "datetimeoffset":
+		case "date", "time", "smalldatetime", "datetime", "datetime2":
 			// TODO: add date & time processing
 			return nil, fmt.Errorf("convert type '%s': %w", typeName, common.ErrDataTypeNotSupported)
 		default:
---
+++ tests/infra/datasource/mssql/init/setup.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS simple;
+GO
+
+CREATE TABLE simple (
+    id INT IDENTITY(1,1) PRIMARY KEY, 
+    col1 VARCHAR(7) NOT NULL,
+    col2 INTEGER NOT NULL
+);
+GO
+
+INSERT INTO simple (col1, col2) VALUES 
+    ('a', 10),
+    ('b', 20),
+    ('c', 30);
+GO
+
+SELECT * FROM simple;
+
+-- ### Numeric Data Types
+-- 1. bit – Integer that can be 0, 1, or NULL.
+-- 2. tinyint – Integer from 0 to 255.
+-- 3. smallint – Integer from -32,768 to 32,767.
+-- 4. int – Integer from -2,147,483,648 to 2,147,483,647.
+-- 5. bigint – Integer from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.
+-- 6. decimal(p, s) – Fixed precision and scale numbers.
+-- 7. numeric(p, s) – Equivalent to decimal.
+-- 8. smallmoney – Monetary data from -214,748.3648 to 214,748.3647.
+-- 9. money – Monetary data from -922,337,203,685,477.5808 to 922,337,203,685,477.5807.
+-- 10. float(n) – Floating point number.
+-- 11. real – Floating point number.
+
+-- ### Date and Time Data Types
+-- 1. date – Stores date data.
+-- 2. time – Stores time of day data.
+-- 3. datetime – Stores date and time data.
+-- 4. datetime2 – Extended date and time data.
+-- 5. smalldatetime – Stores date and time data.
+-- 6. datetimeoffset – Date and time with time zone awareness.
+
+-- ### Character Strings
+-- 1. char(n) – Fixed length non-Unicode data.
+-- 2. varchar(n) – Variable length non-Unicode data.
+-- 3. text – Variable length non-Unicode large data.
+
+-- ### Unicode Character Strings
+-- 1. nchar(n) – Fixed length Unicode data.
+-- 2. nvarchar(n) – Variable length Unicode data.
+-- 3. ntext – Variable length Unicode large data.
+
+-- ### Binary Data Types
+-- 1. binary(n) – Fixed length binary data.
+-- 2. varbinary(n) – Variable length binary data.
+-- 3. image – Variable length binary large data.
+
+-- CREATE TABLE users (
+--     id INT IDENTITY(1,1) NOT NULL,
+--     username NVARCHAR(50) NOT NULL,
+--     password NVARCHAR(50) NOT NULL,
+--     email NVARCHAR(255),
+--     PRIMARY KEY(id)
+-- );
+
+-- INSERT INTO users (username, password, email) VALUES 
+--     ('user1', 'password1', 'user1@email.com'),
+--     ('user2', 'password2', 'user2@email.com'),
+--     ('user3', 'password3', 'user3@email.com'),
+--     ('user4', 'password4', 'user4@email.com'),
+--     ('user5', 'password5', 'user5@email.com'),
+--     ('user6', 'password6', 'user6@email.com'),
+--     ('user7', 'password7', 'user7@email.com'),
+--     ('user8', 'password8', 'user8@email.com'),
+--     ('user9', 'password9', 'user9@email.com'),
+--     ('user10', 'password10', 'user10@email.com');
