25f14673594b6c6e86787dd4e48aa0f8f828f228
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-09 00:21:12 +0300

    Minor changes

--- tests/main_test.go
+++ tests/main_test.go
@@ -6,7 +6,12 @@ import (
 
 	testify_suite "github.com/stretchr/testify/suite"
 
+	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/clickhouse"
+	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/greenplum"
 	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/ms_sql_server"
+	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/mysql"
+	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/postgresql"
+	"github.com/ydb-platform/fq-connector-go/tests/infra/datasource/ydb"
 	"github.com/ydb-platform/fq-connector-go/tests/suite"
 )
 
@@ -24,25 +29,25 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-// func TestClickHouse(t *testing.T) {
-// 	testify_suite.Run(t, clickhouse.NewSuite(suite.NewBase(t, state, "ClickHouse")))
-// }
+func TestClickHouse(t *testing.T) {
+	testify_suite.Run(t, clickhouse.NewSuite(suite.NewBase(t, state, "ClickHouse")))
+}
 
-// func TestPostgreSQL(t *testing.T) {
-// 	testify_suite.Run(t, postgresql.NewSuite(suite.NewBase(t, state, "PostgreSQL")))
-// }
+func TestPostgreSQL(t *testing.T) {
+	testify_suite.Run(t, postgresql.NewSuite(suite.NewBase(t, state, "PostgreSQL")))
+}
 
-// func TestYDB(t *testing.T) {
-// 	testify_suite.Run(t, ydb.NewSuite(suite.NewBase(t, state, "YDB")))
-// }
+func TestYDB(t *testing.T) {
+	testify_suite.Run(t, ydb.NewSuite(suite.NewBase(t, state, "YDB")))
+}
 
-// func TestGreenplum(t *testing.T) {
-// 	testify_suite.Run(t, greenplum.NewSuite(suite.NewBase(t, state, "Greenplum")))
-// }
+func TestGreenplum(t *testing.T) {
+	testify_suite.Run(t, greenplum.NewSuite(suite.NewBase(t, state, "Greenplum")))
+}
 
-// func TestMySQL(t *testing.T) {
-// 	testify_suite.Run(t, mysql.NewSuite(suite.NewBase(t, state, "MySQL")))
-// }
+func TestMySQL(t *testing.T) {
+	testify_suite.Run(t, mysql.NewSuite(suite.NewBase(t, state, "MySQL")))
+}
 
 func TestMsSqlServer(t *testing.T) {
 	testify_suite.Run(t, ms_sql_server.NewSuite(suite.NewBase(t, state, "MS SQL Server")))
