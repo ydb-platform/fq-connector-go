a4071a5bdc9819996afcc0bcfe82bec51dfc18b6
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-21 12:04:09 +0300

    Move retriers to upper directory

--- app/server/datasource/rdbms/clickhouse/retry.go
+++ app/server/datasource/rdbms/clickhouse/retry.go
@@ -6,6 +6,12 @@ import (
 	"syscall"
 )
 
+// //go:linkname decomposeDate time.(*Time).date
+// func decomposeDate(*time.Time, bool) (year int, month int, day int, dayOfYear int)
+
+// //go:linkname formatBits strconv.formatBits
+// func formatBits([]byte, uint64, int, bool, bool) (b []byte, s string)
+
 func RetriableErrorCheckerMakeConnection(err error) bool {
 	if errors.Is(err, os.ErrDeadlineExceeded) {
 		return true
