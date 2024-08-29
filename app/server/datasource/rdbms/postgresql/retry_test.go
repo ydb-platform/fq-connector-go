4dd797a6ee44c784c98d95c74d4da73e751d2415
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-30 01:13:27 +0300

    linter fix

--- app/server/datasource/rdbms/postgresql/retry_test.go
+++ app/server/datasource/rdbms/postgresql/retry_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+//nolint:lll,revive
 func TestDeadlineExceeded(t *testing.T) {
 	err := errors.New("make connection: connect config: failed to connect to `host=172.24.0.3 user=user database=join_pg_pg`: dial error (timeout: context deadline exceeded)")
 	require.True(t, ErrorCheckerMakeConnection(err))
