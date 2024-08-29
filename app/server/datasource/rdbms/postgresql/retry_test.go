121e5e847aaf44280e2c4839b784989c0e76a99d
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-29 12:50:30 +0300

    Improve logger

---
+++ app/server/datasource/rdbms/postgresql/retry_test.go
@@ -0,0 +1,13 @@
+package postgresql
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeadlineExceeded(t *testing.T) {
+	err := errors.New("make connection: connect config: failed to connect to `host=172.24.0.3 user=user database=join_pg_pg`: dial error (timeout: context deadline exceeded)")
+	require.True(t, ErrorCheckerMakeConnection(err))
+}
