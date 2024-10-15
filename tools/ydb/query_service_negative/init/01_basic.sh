c272b59fcc352ed8fe6b011b1d1620254df8e09d
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-10-15 15:37:26 +0300

    More negative tests (#205)

--- tools/ydb/query_service_negative/init/01_basic.sh
+++ tools/ydb/query_service_negative/init/01_basic.sh
@@ -2,14 +2,15 @@
 
 /ydb -p tests-ydb-client yql -s '
 
-    CREATE TABLE simple (id Int32 NOT NULL, col1 String NOT NULL, col2 Int32 NOT NULL, PRIMARY KEY (id));
+    CREATE TABLE simple (id Int32 NOT NULL, col1 String, col2 Int32, PRIMARY KEY (id));
     COMMIT;
     INSERT INTO simple (id, col1, col2) VALUES
       (1, "ydb_a", 10),
       (2, "ydb_b", 20),
       (3, "ydb_c", 30),
       (4, "ydb_d", 40),
-      (5, "ydb_e", 50);
+      (5, "ydb_e", 50),
+      (6, "ydb_g", NULL);
     COMMIT;
   '
   
