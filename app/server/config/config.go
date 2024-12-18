036456ca2b8e531dd1731a1c6c1c26f726807b40
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-12-18 14:46:31 +0300

    Specify IAM default endpoint

--- app/server/config/config.go
+++ app/server/config/config.go
@@ -171,6 +171,15 @@ func fillYdbConfigDefaults(c *config.TYdbConfig) {
 	if c.ExponentialBackoff == nil {
 		c.ExponentialBackoff = makeDefaultExponentialBackoffConfig()
 	}
+
+	if c.ServiceAccountKeyFileCredentials != "" {
+		if c.IamEndpoint == nil {
+			c.IamEndpoint = &api_common.TGenericEndpoint{
+				Host: "iam.api.cloud.yandex.net",
+				Port: 443,
+			}
+		}
+	}
 }
 
 func validateServerConfig(c *config.TServerConfig) error {
@@ -348,6 +357,14 @@ func validateYdbConfig(c *config.TYdbConfig) error {
 
 		if c.IamEndpoint == nil {
 			return fmt.Errorf("you must set `iam_endpoint` if `service_account_key_file_credentials` is set")
+		}
+
+		if c.IamEndpoint.Host == "" {
+			return fmt.Errorf("invalid value of field `iam_endpoint.host`: %v", c.IamEndpoint.Host)
+		}
+
+		if c.IamEndpoint.Port == 0 {
+			return fmt.Errorf("invalid value of field `iam_endpoint.port`: %v", c.IamEndpoint.Port)
 		}
 	}
 
