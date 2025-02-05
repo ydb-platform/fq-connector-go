8fd57d091566944575304f861419bd0e20c3dd7d
Vitaly Isaev <vitalyisaev@ydb.tech>
2025-02-05 18:23:48 +0300

    Refactor pushdown builder (#236)
    * Predicate builder refactoring
    
    * Use OOP for predicate builder
    
    * All tests are passed after refactoring
    
    * Minor changes

--- app/server/datasource/rdbms/utils/query_builder.go
+++ app/server/datasource/rdbms/utils/query_builder.go
@@ -45,23 +45,13 @@ func MakeReadSplitsQuery(
 	if slct.Where != nil {
 		var clause string
 
-		clause, queryArgs, err = formatWhereClause(formatter, slct.Where)
+		clause, queryArgs, err = formatWhereClause(logger, filtering, formatter, slct.Where)
 		if err != nil {
-			switch filtering {
-			case api_service_protos.TReadSplitsRequest_FILTERING_UNSPECIFIED, api_service_protos.TReadSplitsRequest_FILTERING_OPTIONAL:
-				// Pushdown error is suppressed in this mode. Connector will ask for table full scan,
-				// and it's YDB is in charge for appropriate filtering
-				logger.Warn("Failed to format WHERE clause", zap.Error(err), zap.String("where", slct.Where.String()))
-			case api_service_protos.TReadSplitsRequest_FILTERING_MANDATORY:
-				// Pushdown is mandatory in this mode.
-				// If connector doesn't support some types or expressions, the request will fail.
-				return nil, fmt.Errorf("failed to format WHERE clause: %w", err)
-			default:
-				return nil, fmt.Errorf("unknown filtering mode: %d", filtering)
-			}
-		} else {
-			sb.WriteString(" ")
-			sb.WriteString(clause)
+			return nil, fmt.Errorf("format where clause: %w", err)
+		}
+
+		if len(clause) != 0 {
+			sb.WriteString(" WHERE " + clause)
 		}
 	}
 
