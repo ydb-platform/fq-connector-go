282897326ec0271170eb6ef6f6c543d19539e4a4
Arslanka <ars2003lan@gmail.com>
2025-05-04 18:29:23 +0300

    rebased

--- app/server/datasource/nosql/opensearch/query_builder.go
+++ app/server/datasource/nosql/opensearch/query_builder.go
@@ -5,11 +5,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"time"
+
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
+	"go.uber.org/zap"
+
+	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb"
+
+	api_service_protos "github.com/ydb-platform/fq-connector-go/api/service/protos"
+	"github.com/ydb-platform/fq-connector-go/common"
 )
 
-type queryBuilder struct{}
+type queryBuilder struct {
+	logger *zap.Logger
+}
 
-func (*queryBuilder) buildInitialSearchQuery(batchSize uint64) (io.Reader, error) {
+func newQueryBuilder(logger *zap.Logger) *QueryBuilder {
+	return &QueryBuilder{logger: logger}
+}
+
+func (qb *QueryBuilder) BuildSearchQuery(
+	split *api_service_protos.TSplit,
+	filtering api_service_protos.TReadSplitsRequest_EFiltering,
+	batchSize uint64,
+	scrollTimeout time.Duration,
+) (io.Reader, *opensearchapi.SearchParams, error) {
+	params := &opensearchapi.SearchParams{
+		Scroll: scrollTimeout,
+	}
+
+	what := split.Select.GetWhat()
+	if what == nil {
+		return nil, nil, fmt.Errorf("not specified columns to query in Select.What")
+	}
+
+	var projection []string
+	for _, item := range what.GetItems() {
+		projection = append(projection, item.GetColumn().Name)
+	}
+
+func (*QueryBuilder) BuildInitialSearchQuery(batchSize uint64) (io.Reader, error) {
 	query := map[string]any{
 		"size": batchSize,
 		"query": map[string]any{
